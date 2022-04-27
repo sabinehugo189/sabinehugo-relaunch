16b72b0040c7df54a16c2cbca75769726815e429
Stefan Frede <sfr@wyn.rocks>
2022-04-27 11:10:29 +0000

    feat: add contact teaser section

---
+++ components/c-contact-teaser.vue
@@ -0,0 +1,42 @@
+<template>
+  <section class="container">
+    <p>
+      {{ props.teaser }} Gerne berate ich Sie ausführlich. Machen Sie heute noch
+      einen Termin in unserer Fachpraxis für Invisalign in Würzburg aus.
+    </p>
+    <NuxtLink
+      to="#contact"
+      class="btn"
+    >
+      Jetzt Termin vereinbaren
+    </NuxtLink>
+  </section>
+</template>
+
+<script setup>
+const props = defineProps({
+  teaser: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<style scoped>
+.container {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  gap: var(--size-10);
+  margin-inline: auto;
+  max-inline-size: var(--size-md);
+  text-align: center;
+}
+
+p {
+  color: var(--link);
+  font-size: var(--font-size-fluid-1);
+  font-weight: var(--font-weight-8);
+  line-height: var(--font-lineheight-1);
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,13 @@
   <c-hero />
   <NuxtLayout name="default">
     <c-usp />
+    <c-contact-teaser teaser="Sie wünschen sich ein schönes Lachen?" />
+    <c-contact-teaser
+      teaser="Sie interessieren sich für eine Behandlung mit Invisalign?"
+    />
+    <c-contact-teaser
+      teaser="Sie haben eine Frage zum Ablauf der Invisalign Behandlung?"
+    />
     <c-contact />
   </NuxtLayout>
 </template>
