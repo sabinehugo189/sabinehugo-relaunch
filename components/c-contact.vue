009fea5379ce518139b269d14dcc98a72e86e2d1
Stefan Frede <sfr@wyn.rocks>
2022-05-10 17:02:25 +0200

    feat(form): implement form submit
    SH-5

--- components/c-contact.vue
+++ components/c-contact.vue
@@ -15,7 +15,33 @@
         Sie uns eine E-Mail. Wir melden uns innerhalb eines Werktags bei Ihnen.
       </p>
     </header>
-    <form @submit="submit">
+    <form
+      ref="form"
+      data-netlify="true"
+      name="contact"
+      method="POST"
+      @submit="submit"
+    >
+      <div
+        v-if="notification"
+        class="notification"
+      >
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+          >
+            <!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+            <path
+              d="M0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256zM371.8 211.8C382.7 200.9 382.7 183.1 371.8 172.2C360.9 161.3 343.1 161.3 332.2 172.2L224 280.4L179.8 236.2C168.9 225.3 151.1 225.3 140.2 236.2C129.3 247.1 129.3 264.9 140.2 275.8L204.2 339.8C215.1 350.7 232.9 350.7 243.8 339.8L371.8 211.8z"
+            />
+          </svg>
+        </div>
+        <div>
+          <h4>Vielen Dank für Ihre Nachricht</h4>
+          <p>Wir werden uns schnellstmöglich bei Ihnen zurück melden.</p>
+        </div>
+      </div>
       <div class="salutation">
         <h4>Anrede</h4>
         <c-radio-group
@@ -81,6 +107,9 @@
 import { useField, useForm } from 'vee-validate';
 import { object, string, boolean } from 'yup';
 
+const form = ref(null);
+const notification = ref(false);
+
 const options = [
   { label: 'Frau', value: 'Frau' },
   { label: 'Herr', value: 'Herr' },
@@ -120,9 +149,24 @@ const { value: callback } = useField('callback');
 const { value: privacy } = useField('privacy');
 
 const submit = handleSubmit((values, { resetForm }) => {
-  console.log('SUBMIT: ', values);
+  const body = Object.keys(values).reduce((prev, curr) => {
+    return prev.concat(
+      '&',
+      `${encodeURIComponent(curr)}=${encodeURIComponent(values[curr])}`,
+    );
+  }, `form-name=${encodeURIComponent(form.value.name)}`);
 
-  resetForm();
+  fetch('/', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body,
+  })
+    .then(() => {
+      notification.value = true;
+
+      return resetForm();
+    })
+    .catch((error) => console.error('Es ist ein Fehler aufgetreten: ', error));
 });
 </script>
 
@@ -152,6 +196,23 @@ p {
 form {
   display: grid;
   gap: var(--size-16);
+}
+
+.notification {
+  background-color: var(--green-6);
+  border-radius: var(--radius-3);
+  color: var(--gray-0);
+  display: flex;
+  gap: var(--size-8);
+  padding: var(--size-5) var(--size-8);
+}
+
+.notification h4 {
+  color: var(--gray-0);
+}
+
+.notification svg {
+  fill: currentColor;
 }
 
 .salutation {
