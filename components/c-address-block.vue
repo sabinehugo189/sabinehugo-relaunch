ccdf8adb24722ddd71e52b6a189ac3a1d3e04274
Stefan Frede <stefan@frede.dev>
2022-05-18 17:10:27 +0200

    feat(footer): implement change requests (#10)
    SH-5
    
    Co-authored-by: Stefan Frede <sfr@wyn.rocks>

---
+++ components/c-address-block.vue
@@ -0,0 +1,54 @@
+<template>
+  <address class="h-card">
+    <p class="p-note">Zahn &amp; Ästhetik</p>
+    <p class="p-name">Dr. med. Sabine Hugo</p>
+    <p class="h-adr">
+      <span class="p-street-address">Berliner Platz 9</span>
+      <span>
+        <span class="p-postal-code">97080</span>
+        <span class="p-locality">Würzburg</span>
+      </span>
+    </p>
+    <p>
+      <span class="p-tel">+49 931 99 147 070</span>
+      <span class="u-email">info@praxishugo.de</span>
+    </p>
+  </address>
+  <address class="h-card">
+    <p class="p-name">Hugo & Kollegen</p>
+    <p class="h-adr">
+      <span class="p-street-address">Am Zeughaus 40</span>
+      <span>
+        <span class="p-postal-code">97421</span>
+        <span class="p-locality">Schweinfurt</span>
+      </span>
+    </p>
+  </address>
+</template>
+
+<style scoped>
+address {
+  display: flex;
+  flex-direction: column;
+  font-style: normal;
+}
+
+address > * {
+  margin-block: 0;
+}
+
+address > p:nth-child(4) {
+  margin-block-start: var(--size-4);
+}
+
+address span {
+  display: flex;
+  gap: 0.5em;
+}
+
+@media (min-width: 1280px) {
+  address > *:nth-child(4) {
+    margin-block-start: var(--size-12);
+  }
+}
+</style>
---
+++ components/c-link-list.vue
@@ -0,0 +1,40 @@
+<template>
+  <ul class="link-list">
+    <li><a href="#">Impressum</a></li>
+    <li><a href="#">Datenschutz</a></li>
+  </ul>
+</template>
+
+<style scoped>
+.link-list {
+  display: flex;
+  flex-direction: column;
+  gap: normal;
+  list-style: none;
+  padding-inline-start: 0;
+}
+
+.link-list > li {
+  display: initial;
+  gap: normal;
+  padding-inline-start: 0;
+}
+
+.link-list > li > a {
+  background-color: transparent;
+  color: inherit;
+  text-decoration: none;
+}
+
+.link-list > li > a:hover {
+  text-decoration-line: underline;
+  text-decoration-style: solid;
+  text-decoration-thickness: 3px;
+  text-underline-position: under;
+}
+
+.link-list > li > a:focus-visible {
+  outline-color: var(--text-1);
+  outline-style: solid;
+}
+</style>
