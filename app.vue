7bda929a0d8f0d7c86b04ddfbb5da8b92f2d3dd1
Stefan Frede <sfr@wyn.rocks>
2022-05-20 10:49:06 +0200

    refactor(layout): add single root element
    SH-5

--- app.vue
+++ app.vue
@@ -1,17 +1,18 @@
 <template>
-  <div
-    ref="target"
-    class="sentinel"
-  ></div>
-  <c-header :is-stuck="isHidden" />
-  <main>
-    <NuxtPage />
-  </main>
-  <c-footer />
+  <div class="root">
+    <div
+      ref="target"
+      class="sentinel"
+    ></div>
+    <c-header :is-stuck="isHidden" />
+    <main>
+      <NuxtPage />
+    </main>
+    <c-footer />
+  </div>
 </template>
 
 <script setup>
-/* global useIntersectionObserver */
 const target = ref(null);
 const isHidden = ref(false);
 
@@ -24,5 +25,18 @@ useIntersectionObserver(target, ([{ isIntersecting }]) => {
 .sentinel {
   background-color: transparent;
   height: var(--size-px);
+}
+
+.root {
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+  padding-block: calc(var(--size-5) - var(--size-px)) var(--size-5);
+  position: relative;
+}
+
+@media (min-width: 1280px) {
+  .root {
+    padding-block: calc(var(--size-10) - var(--size-px)) var(--size-10);
+  }
 }
 </style>
