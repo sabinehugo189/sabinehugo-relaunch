dca4efe1f2f5253def04f2e5c60deec46f84f557
Stefan Frede <sfr@wyn.rocks>
2022-05-23 20:43:40 +0200

    feat(vimeo): load vimeo player through a facade
    SH-5

--- components/c-vimeo-player.vue
+++ components/c-vimeo-player.vue
@@ -1,16 +1,37 @@
 <template>
-  <client-only>
-    <vue-vimeo-player
-      :video-id="props.videoId"
-      :options="options"
-      player-height="365"
-      player-width="650"
-    >
-    </vue-vimeo-player>
-  </client-only>
+  <div
+    ref="root"
+    class="vimeo"
+    :class="{ 'is-ready': ready }"
+  >
+    <img
+      :src="src"
+      :srcset="`
+        ${src850} 850w,
+        ${src550} 550w
+      `"
+      alt="2 Frauen in einer Praxis gucken gemeinsam auf einen Monitor"
+      width="650"
+      height="365"
+      sizes="(min-width: 1280px) 550px, (min-width: 640px) 850px, 100vw"
+    />
+    <template v-if="toggleVideo">
+      <client-only>
+        <vue-vimeo-player
+          :video-id="props.videoId"
+          :options="options"
+          player-width="650"
+          player-height="365"
+          @ready="onReady"
+        >
+        </vue-vimeo-player>
+      </client-only>
+    </template>
+  </div>
 </template>
 
 <script setup>
+import { buildImageUrl } from 'cloudinary-build-url';
 import { vueVimeoPlayer } from 'vue-vimeo-player';
 
 const props = defineProps({
@@ -20,7 +41,76 @@ const props = defineProps({
   },
 });
 
+const ready = ref('false');
+const root = ref(null);
+const toggleVideo = ref(false);
+
 const options = {
   responsive: true,
 };
+
+useIntersectionObserver(root, ([{ isIntersecting }]) => {
+  if (!toggleVideo.value) {
+    toggleVideo.value = isIntersecting;
+  }
+});
+
+const onReady = () => {
+  ready.value = true;
+};
+
+const url =
+  'https://res.cloudinary.com/zahn-und-sthetik/image/upload/v1652359235/invisalign/video.jpg';
+const cloudName = 'zahn-und-sthetik';
+const resize = { type: 'scale', aspectRatio: '1.618' };
+
+const src = buildImageUrl(url, {
+  cloud: {
+    cloudName,
+  },
+  transformations: {
+    resize: {
+      ...resize,
+      width: 600,
+    },
+  },
+});
+
+const src550 = buildImageUrl(url, {
+  cloud: {
+    cloudName,
+  },
+  transformations: {
+    resize: {
+      ...resize,
+      width: 550,
+    },
+  },
+});
+
+const src850 = buildImageUrl(url, {
+  cloud: {
+    cloudName,
+  },
+  transformations: {
+    resize: {
+      ...resize,
+      width: 850,
+    },
+  },
+});
 </script>
+
+<style scoped>
+.vimeo {
+  aspect-ratio: 1.777 / 1;
+  position: relative;
+}
+
+.vimeo > img {
+  height: 100%;
+  object-fit: cover;
+  position: absolute;
+  width: 100%;
+}
+</style>
