9e538c35f9a6827596eda2bfdd08a2f2e004f3e4
Stefan Frede <sfr@wyn.rocks>
2022-05-25 14:43:42 +0200

    feat: install content module
    SH-5

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ import { defineNuxtConfig } from 'nuxt';
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
-  modules: ['@vueuse/nuxt'],
+  modules: ['@nuxt/content', '@vueuse/nuxt'],
   typescript: {
     shim: false,
   },
