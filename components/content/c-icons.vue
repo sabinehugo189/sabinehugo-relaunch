0d90a08b8afe02e5d91ae11e4ff77e11f903ff49
Stefan Frede <sfr@wyn.rocks>
2022-05-30 21:10:04 +0200

    feat(content): make icon teaser maintainable
    SH-5

---
+++ components/content/c-icons.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="container">
+    <ul class="icons">
+      <li><slot name="icon-01" /></li>
+      <li><slot name="icon-02" /></li>
+      <li><slot name="icon-03" /></li>
+      <li><slot name="icon-04" /></li>
+    </ul>
+  </section>
+</template>
+
+<style scoped>
+.icons {
+  display: grid;
+  gap: var(--size-5);
+  justify-content: center;
+  list-style: none;
+  margin-inline: auto;
+  max-inline-size: var(--size-xxl);
+  padding-inline-start: 0;
+}
+
+@media (min-width: 768px) {
+  .icons {
+    gap: var(--size-8);
+    grid-template-columns: repeat(2, 1fr);
+  }
+}
+
+@media (min-width: 1280px) {
+  .icons {
+    grid-template-columns: repeat(auto-fit, minmax(15rem, 1fr));
+  }
+}
+</style>
