412a7d0e826094ea67035f10c4d51480175422fb
Stefan Frede <sfr@wyn.rocks>
2022-06-24 11:54:00 +0200

    refactor: update data query
    Query explicitly for partials.

--- components/c-hero.vue
+++ components/c-hero.vue
@@ -60,6 +60,7 @@ const src = buildImageUrl(`${urlPart}quality-seal.png`, {
 
 const { data } = await useAsyncData('hero', () => {
   return queryContent('/')
+    .where({ _partial: true })
     .only(['title', 'subtitle', 'description', 'label'])
     .findOne();
 });
