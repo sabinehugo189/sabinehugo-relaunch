a406949d9888d5d8d95a143beabad69993811a55
Stefan Frede <sfr@wyn.rocks>
2022-06-30 13:21:12 +0200

    fix(layout): adjuxt padding for default layouts
    SH-15

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,33 +1,69 @@
 <template>
-  <div class="wrapper">
+  <div
+    class="wrapper"
+    :class="{
+      'is-plain': isPlain,
+    }"
+  >
     <div class="container">
       <slot />
     </div>
   </div>
 </template>
 
+<script setup>
+const route = useRoute();
+const [slug = 'home'] = route.params.slug;
+const isPlain = ref(slug === 'imprint' || slug === 'privacy' ? true : false);
+
+watch(
+  () => route.params.slug,
+  ([slug]) => {
+    switch (slug) {
+      case 'imprint':
+      case 'privacy':
+        isPlain.value = true;
+        break;
+
+      default:
+        isPlain.value = false;
+        break;
+    }
+  },
+);
+</script>
+
 <style scoped>
 .wrapper {
-  padding: var(--size-20) var(--size-5) 0;
+  padding-inline: var(--size-5);
+}
+
+.wrapper.is-plain {
+  padding-block-start: var(--size-16);
 }
 
 .container {
   display: flex;
   flex-direction: column;
   margin-inline: auto;
-  max-inline-size: var(--size-xxl);
+  max-inline-size: var(--size-xxxl);
   padding-inline: var(--size-5);
-  row-gap: var(--size-10);
+  row-gap: var(--size-48);
 }
 
 @media (min-width: 1280px) {
   .wrapper {
-    padding-inline: var(--size-10);
+    padding-inline: var(--size-30);
   }
 
   .container {
     padding-inline: 0;
-    row-gap: var(--size-40);
+  }
+}
+
+@media (min-width: 1536px) {
+  .wrapper {
+    padding-inline: var(--size-50);
   }
 }
 </style>
