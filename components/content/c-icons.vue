ab54ec51760303d3bda3807737d3e759ad7e403a
Stefan Frede <stefan@frede.dev>
2022-07-02 12:14:30 +0200

    refactor(modules): adjust icons (#64)
    Add an optional header and switch to svg images on cloudinary.
    
    SH-15
    
    Co-authored-by: Stefan Frede <sfr@wyn.rocks>

--- components/content/c-icons.vue
+++ components/content/c-icons.vue
@@ -1,40 +1,85 @@
 <template>
   <section class="container">
+    <header v-if="title">
+      <h3>
+        {{ title }}
+      </h3>
+    </header>
     <ul class="icons">
-      <li><slot name="icon-01" /></li>
-      <li><slot name="icon-02" /></li>
-      <li><slot name="icon-03" /></li>
-      <li><slot name="icon-04" /></li>
+      <li
+        v-for="(icon, index) in icons"
+        :key="`icon-${uid}-${index}`"
+        class="icon"
+      >
+        <c-icon
+          :caption="icon.caption"
+          :icon-name="icon.iconName"
+        />
+      </li>
     </ul>
   </section>
 </template>
 
+<script setup>
+defineProps({
+  icons: {
+    type: Object,
+    required: true,
+  },
+  title: {
+    type: String,
+    default: '',
+  },
+});
+
+const { uid } = getCurrentInstance();
+</script>
+
 <style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  gap: var(--size-10);
+  width: 100%;
+}
+
+@media (min-width: 1280px) {
+  .container {
+    padding-inline: var(--size-20);
+  }
+}
+
+@media (min-width: 1536px) {
+  .container {
+    padding-inline: var(--size-40);
+  }
+}
+
+header {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+}
+
+h3 {
+  font-size: var(--font-size-fluid-3);
+}
+
 .icons {
   display: grid;
-  gap: var(--size-5);
-  justify-content: center;
+  gap: var(--size-8);
   list-style: none;
-  margin-inline: calc(var(--size-5) * -1);
-  max-inline-size: var(--size-xxl);
   padding-inline-start: 0;
 }
 
-.icons > li {
+.icon {
   padding-inline-start: 0;
 }
 
 @media (min-width: 768px) {
   .icons {
-    gap: var(--size-8);
-    grid-template-columns: repeat(2, 1fr);
-  }
-}
-
-@media (min-width: 1280px) {
-  .icons {
-    grid-template-columns: repeat(auto-fit, minmax(15rem, 1fr));
-    margin-inline: auto;
+    grid-template-columns: repeat(auto-fit, minmax(14rem, 1fr));
+    justify-content: center;
   }
 }
 </style>
