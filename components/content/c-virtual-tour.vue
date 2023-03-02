a7b9c0788fc76d1cb680c652e5593fb53e456ac8
Stefan Frede <sfr@wyn.rocks>
2023-03-02 09:49:34 +0100

    feat(components): add c-virtual tour
    SH-82

---
+++ components/content/c-virtual-tour.vue
@@ -0,0 +1,35 @@
+<template>
+  <iframe
+    :src="url"
+    title="Virtueller Rundgang"
+    width="640"
+    height="480"
+  ></iframe>
+</template>
+
+<script setup>
+defineProps({
+  url: {
+    type: String,
+    default: '',
+  },
+});
+</script>
+
+<style scoped>
+iframe {
+  inline-size: 100%;
+}
+
+@media (orientation: landscape) {
+  iframe {
+    aspect-ratio: 4 / 3;
+  }
+}
+
+@media (orientation: portrait) {
+  iframe {
+    aspect-ratio: 3 / 4;
+  }
+}
+</style>
